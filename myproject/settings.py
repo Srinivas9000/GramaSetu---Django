4f907f79904edbba778664bd4bd346e27c3062e1
Srinivas Manya <srinivasmanya2000@gmail.com>
2026-03-14 16:56:18 +0530

    add database configuration

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # APPLICATIONS
@@ -80,8 +80,9 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 if ENVIRONMENT == "production":
     DATABASES = {
         "default": dj_database_url.config(
-            default=os.getenv("DATABASE_URL")
-            conn_max_age=600
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
         )
     }
 else:
